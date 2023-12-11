dcbfc992685a086f44eadb0895b6cdd74780b097
John-LP <jonathanleplat@hotmail.fr>
2023-12-11 22:12:01 +0100

    modification de postedCard pour map la tables locations et afficher le contenu dans Home, searchBar 100% opérationnelle

--- src/Component/SearchBar/SearchBar.jsx
+++ src/Component/SearchBar/SearchBar.jsx
@@ -1,4 +1,5 @@
 import "./SearchBar.css";
+import axios from 'axios';
 import * as React from "react";
 import { useNavigate } from "react-router-dom";
 import { IonIcon } from '@ionic/react';
@@ -6,30 +7,59 @@ import { wine, bed, restaurant, search } from 'ionicons/icons';
 
 export default function SearchBar() {
   const [selectedOption, setSelectedOption] = React.useState(null);
+  const [searchText, setSearchText] = React.useState('');
+
+  const handleInputChange = (event) => {
+    setSearchText(event.target.value);
+  };
 
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
   };
 
-  const performAction = () => {
-    switch (selectedOption) {
-      case 'hotel':
-        console.log('Option Hôtel selected');
-        // Code requête à Laravel pour choix de l'option Hôtel (category_id = 1)
-        break;
-      case 'bar':
-        console.log('Option Bar selected');
-        // Code requête à Laravel pour choix de l'option Bar (category_id = 2)
-        break;
-      case 'restaurant':
-        console.log('Option Restaurant selected');
-        // Code requête à Laravel pour choix de l'option Restaurant (category_id = 3)
-        break;
-      default:
-        console.log('Aucune option sélectionnée');
-        break;
-    }
-  };
+  const performAction = async () => {
+  switch (selectedOption) {
+    case 'hotel':
+      console.log('Option Hôtel selected');
+      try {
+        const response = await axios.post('http://localhost:8000/api/1', {
+          category_id: 1,
+          searchText: searchText,
+        });
+        console.log(response.data);
+      } catch (error) {
+        console.error('Erreur lors de la requête Hôtel', error);
+      }
+      break;
+    case 'bar':
+      console.log('Option Bar selected');
+      try {
+        const response = await axios.post('http://localhost:8000/api/2', {
+          category_id: 2,
+          searchText: searchText,
+        });
+        console.log(response.data);
+      } catch (error) {
+        console.error('Erreur lors de la requête Bar', error);
+      }
+      break;
+    case 'restaurant':
+      console.log('Option Restaurant selected');
+      try {
+        const response = await axios.post('http://localhost:8000/api/3', {
+          category_id: 3,
+          searchText: searchText,
+        });
+        console.log(response.data);
+      } catch (error) {
+        console.error('Erreur lors de la requête Restaurant', error);
+      }
+      break;
+    default:
+      console.log('Aucune option sélectionnée');
+      break;
+  }
+};
 
   const getSearchPlaceholder = () => {
     switch (selectedOption) {
@@ -47,7 +77,8 @@ export default function SearchBar() {
   return (
     <div>
       <div className="search-bar">
-        <input className="searchInput" type="text" placeholder={getSearchPlaceholder()} />
+        <input className="searchInput" type="text" placeholder={getSearchPlaceholder()} value={searchText}
+          onChange={handleInputChange}/>
         <button className="iconeLoupe" type="button" onClick={performAction}>
           <IonIcon className="icon-filter-search" icon={search} />
         </button>

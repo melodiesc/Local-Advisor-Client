d865f8051ea34b17fb03f37dcd3489dc1ca4bdb0
John-LP <jonathanleplat@hotmail.fr>
2023-12-18 16:24:31 +0100

    CSS affichage detaisl

--- src/Component/Details/Details.jsx
+++ src/Component/Details/Details.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
 import {Container,Typography,Box,CardMedia,CircularProgress,} from "@mui/material";
+import "./Details.css";
 
 function Details() {
   const { id } = useParams();
@@ -42,34 +43,39 @@ function Details() {
 
   return (
     <div>
-    <NavBar />
-    <Container component="main" maxWidth="md">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="body1">
-          <h2> {details.category.category}</h2>
-        </Typography>
+      <NavBar />
+        <div className="parentDetails">
+          <div className="details">
+            <Container component="main" maxWidth="md">
+              <Box sx={{ my: 4 }}>
+                <Typography variant="body1">
+                  <h1 className="titre">{details.category.category} {details.name}</h1>
+                </Typography>
 
-        <Typography variant="body1">
-          Adresse: {details.address}, {details.zip_code} {details.city}
-        </Typography>
-        {details.image_path && (
-          <CardMedia
-            component="img"
-            image={details.image_path}
-            alt={`Photo du lieu ${details.name}`}
-            sx={{ width: "100%", mb: 2 }}
-          />
-        )}
-        <Typography variant="body1">
-          Note: {details.rate ? details.rate.name : "Non spécifié"}
-        </Typography>
-        {/* <Typography variant="body1">Note: {details.rate.rate}</Typography> */}
+                {details.image_path && (
+                  <CardMedia
+                    component="img"
+                    className="imageDetails"
+                    image={details.image_path}
+                    alt={`Photo du lieu ${details.name}`}
+                    sx={{ width: "100%", mb: 2 }}
+                  />
+                )}
+                <Typography variant="body1">
+                <p className="address"><u>Adresse:</u> {details.address}, {details.zip_code} {details.city}</p>
+                </Typography>
+                <Typography variant="body1">
+                <p className="rate">{details.rate ? details.rate.name : "Non spécifié"}</p>
+                </Typography>
+                {/* <Typography variant="body1">Note: {details.rate.rate}</Typography> */}
 
-        <Typography variant="body1" sx={{ mb: 2 }}>
-          Description: {details.description}
-        </Typography>
-      </Box>
-    </Container>
+                <Typography variant="body1" sx={{ mb: 2 }}>
+                  <p className="description">{details.description}</p>
+                </Typography>
+              </Box>
+            </Container>
+          </div>
+        </div>
     </div>
   );
 }
